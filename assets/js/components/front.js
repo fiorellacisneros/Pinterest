29869f7b9ab364c851e6fe14cd9812befde6a654
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-06 12:29:44 -0500

    add updates

--- assets/js/components/front.js
+++ assets/js/components/front.js
@@ -4,7 +4,7 @@ const frontPage = () => {
     const pines = $('<p class="front__pines"><strong>'+ state.data.data.counts.pins +'</strong> Pines</p>');
     const followers = $('<p class="front__pines"><strong>'+ state.data.data.counts.followers +'</strong> Seguidores</p>');
     const imguserprinc = $('<img class="front__userprinc" alt="arabela" role="presentation" src="https://s-media-cache-ak0.pinimg.com/avatars/arabelyuska_1476548892_75.jpg">');
-    const follow = $('<p class="front__pines"><strong>'+ state.data.data.counts.followers +'</strong> Seguidores</p>');s
+    const follow = $('<p class="front__pines"><strong>'+ state.data.data.counts.followers +'</strong> Seguidores</p>'); 
     
     front.append(titleboard);
     front.append(pines);

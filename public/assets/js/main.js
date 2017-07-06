1dfbd0ebad67d42c4908f278ef38186f418ae072
flor condori <condorihuamanif@gmail.com>
2017-07-06 05:09:05 -0500

    corrigiendo algunos estilos de bootstrap

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,6 +14,8 @@ $(_=>{
 	render(root);
 });
 
+
+
 'use strict';
 const Header = ()=>{
 	const header = $("<header></header>");
@@ -61,8 +63,3 @@ const Header = ()=>{
 
 	return header;
 }
-
-'use strict';
-$.getJSON('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/?access_token=ARstBy7ZbBhQXgzK2Ggf4NkvNwi4FM6MILFo0N9EJCRuwuA9dgAAAAA&fields=id%2Cname%2Curl%2Cdescription%2Ccreator%2Ccreated_at%2Ccounts%2Creason%2Cprivacy%2Cimage', (data)=>{
-	console.log(data);
-});
--- src/assets/js/utils/get-json.js
+++ src/assets/js/utils/get-json.js
@@ -1,4 +1,1 @@
-'use strict';
-$.getJSON('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/?access_token=ARstBy7ZbBhQXgzK2Ggf4NkvNwi4FM6MILFo0N9EJCRuwuA9dgAAAAA&fields=id%2Cname%2Curl%2Cdescription%2Ccreator%2Ccreated_at%2Ccounts%2Creason%2Cprivacy%2Cimage', (data)=>{
-	console.log(data);
-});
+

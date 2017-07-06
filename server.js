14153a230bdcbbfbb1663dddfae401aa8562a770
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-06 18:26:43 -0500

    seteando server

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const app =  express();
 
 app.use('/', express.static('public'));
 
-const port = process.env.PORT || 5000;
-const hostname = '127.0.0.1';
+//const port = process.env.PORT || 5000;
+//const hostname = '127.0.0.1';
 
-app.listen(port, hostname, () => {
-console.log(`Server started ${port}`);
-});
+//app.listen(port, hostname, () => {
+//console.log(`Server started ${port}`);
+//});
+
+
+app.set('port',(process.env.PORT || 5000));
+
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'));
+})

327ced9d4312dd3521b4ca73addd6cb0b356b893
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-06 03:45:48 -0500

    add color boardname

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,7 @@ const state = {
 
 $( _ => {
     
-    getJSON('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/?access_token=ASmqb97ggeIaM18Y4T-0dv_E0Gf3FM6RYQp1a4JEIu4X1ABEigAAAAA&fields=id%2Cname%2Curl%2Ccounts%2Ccreated_at%2Ccreator%2Cdescription%2Cimage%2Cprivacy%2Creason', (err, json) => {
+    getJSON('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/?access_token=AT9rzjJsDsmyps0SBDfKAUiVAU_hFM6l7xHG0ThEJCRuwuA9dgAAAAA&fields=id%2Cname%2Curl%2Ccounts%2Ccreated_at%2Ccreator%2Cdescription%2Cimage%2Cprivacy%2Creason', (err, json) => {
 
         if (err) { return alert(err.message);}
 

33049539172fed2850889c6c42bfb3d22b272051
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-06 02:33:28 -0500

    add fixed

--- assets/js/components/scroll.js
+++ assets/js/components/scroll.js
@@ -1,10 +1,31 @@
 const Scroll = () => {
-    const contscroll = $('<div class="scroll"></div>')
-    const shared = $('<div class="scroll__ico"><i class="icon-share-alternitive"></i></div>');
-    const quit = $('<button type="button" class="weight-800 btn right">Dejar de seguir tablero</button>');
-    
-    contscroll.append(shared);
-    contscroll.append(quit);
-    
+    const contscroll = $('<div class="contscroll"></div>');
+    const fixed = $('<div class="contscroll__fixed"></div>');
+    const shared = $('<div class="contscroll__fixed--ico"><i class="icon-share-alternitive"></i></div>');
+    const nameboard = $('<span class="nameboard weight-800">'+ state.data.data.name+'</span>')
+    const quit = $('<button type="button" class="weight-800 contscroll__fixed__btn btn right">Dejar de seguir tablero</button>');
+
+
+    fixed.append(shared);
+    fixed.append(nameboard);
+    fixed.append(quit);
+    contscroll.append(fixed);
+
+    $(document).ready(function(){
+        $('.nameboard').css({"opacity":0});
+        $(document).scroll(function(){
+            if($(window).scrollTop()>45){
+                $('.nameboard').show();
+                $('.nameboard').css({"transform": "translate(0, 20px)"});
+                $('.nameboard').css({"transition": "300ms ease-out"});
+            }else{
+                $('.nameboard').hide();
+                $('.nameboard').css({"opacity":1});
+                $('.nameboard').css({"font-size": "27px"});
+                $('.nameboard').css({"transform": "translate(0, 0)"});
+                $('.nameboard').css({"transition": "300ms ease-out"});
+            } 
+        });
+    });
     return contscroll;
 } 

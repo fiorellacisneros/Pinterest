128c34f20e52975d6766f1acf03c8111340adad4
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-06 09:13:22 -0500

    add button visile-xs

--- assets/js/components/scroll.js
+++ assets/js/components/scroll.js
@@ -1,32 +1,39 @@
 const Scroll = () => {
-    const contscroll = $('<div class="contscroll"></div>');
+    const contprincipal = $('<div class="contprinc"></div>');
+    const contscroll = $('<div class="contscroll hidden-xs"></div>');
     const fixed = $('<div class="contscroll__fixed"></div>');
     const shared = $('<div class="contscroll__fixed--ico"><i class="icon-share-alternitive"></i></div>');
-    const nameboard = $('<span class="nameboard weight-800">'+ state.data.data.name+'</span>')
-    const quit = $('<button type="button" class="weight-800 contscroll__fixed__btn btn right">Dejar de seguir tablero</button>');
-
-
+    const nameboard = $('<span class="contscroll__fixed--nameboard weight-800">'+ state.data.data.name+'</span>')
+    const quit = $('<button type="button" class="weight-800 contscroll__fixed--btn btn btn-default right">Dejar de seguir tablero</button>');
+    const contleft = $('<div class="contleft"></div>');
+    const left = $('<button type="button" class="weight-800 contleft__btn visible-xs btn btn-default">Dejar de seguir tablero</button>');
+    
     fixed.append(shared);
     fixed.append(nameboard);
     fixed.append(quit);
     contscroll.append(fixed);
+    contprincipal.append(contscroll);
+    contleft.append(left);
+    contprincipal.append(contleft);
 
     $(document).ready(function(){
-        $('.nameboard').css({"opacity":0});
+        $('.contscroll__fixed--nameboard').css({"opacity":0});
         $(document).scroll(function(){
-            if($(window).scrollTop()>45){
-                $('.nameboard').show();
-                $('.nameboard').css({"transform": "translate(0, 20px)"});
-                $('.nameboard').css({"transition": "300ms ease-out"});
+            if($(window).scrollTop()>30){
+                $('.contscroll__fixed--nameboard').show();
+                $('.contscroll__fixed--nameboard').css({"transform": "translate(0, 20px)"});
+                $('.contscroll__fixed--nameboard').css({"transition": "300ms ease-out"});
             }else{
-                $('.nameboard').hide();
-                $('.nameboard').css({"opacity":1});
-                $('.nameboard').css({"font-size": "27px"});
-                $('.nameboard').css({"color": "#555"});
-                $('.nameboard').css({"transform": "translate(0, 0)"});
-                $('.nameboard').css({"transition": "300ms ease-out"});
+                $('.contscroll__fixed--nameboard').hide();
+                $('.contscroll__fixed--nameboard').css({"opacity":1});
+                $('.contscroll__fixed--nameboard').css({"font-size": "27px"});
+                $('.contscroll__fixed--nameboard').css({"color": "#555"});
+                $('.contscroll__fixed--nameboard').css({"margin-left": "120px"});
+                $('.contscroll__fixed--nameboard').css({"transform": "translate(0, 0)"});
+                $('.contscroll__fixed--nameboard').css({"transition": "300ms ease-out"});
+                $('.contscroll__fixed--nameboard').css({"letter-spacing": "-1px"});
             } 
         });
     });
-    return contscroll;
+    return contprincipal;
 } 

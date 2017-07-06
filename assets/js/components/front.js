a2707379e3339af3a45e5a743bdc0d30c67fd0fb
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-06 10:03:12 -0500

    refactorizando

--- assets/js/components/front.js
+++ assets/js/components/front.js
@@ -1,19 +1,15 @@
 const frontPage = () => {
-    //const left = $('<button type="button" class="weight-800 visible-xs contscroll__fixed--btnresp btn btn-default right">Dejar de seguir tablero</button>');
     const front = $('<div class="front"></div>');
     const titleboard = $('<h1 class="front__title weight-800">'+ state.data.data.name +'<h1>');
     const pines = $('<p class="front__pines"><strong>'+ state.data.data.counts.pins +'</strong> Pines</p>');
     const followers = $('<p class="front__pines"><strong>'+ state.data.data.counts.followers +'</strong> Seguidores</p>');
     const imguserprinc = $('<img class="front__userprinc" alt="arabela" role="presentation" src="https://s-media-cache-ak0.pinimg.com/avatars/arabelyuska_1476548892_75.jpg">');
-    const follow = $('<p class="front__pines"><strong>'+ state.data.data.counts.followers +'</strong> Seguidores</p>');
-    const prueba = $('<br><br><br><br><br><br><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur a dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, viLorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi sit ab id ex, commodi quia eveniet beatae nihil natus reprehenderit tenetur, illo minima, vitae nemo. Illum recusandae atque aspernatur sit?</p>');
+    const follow = $('<p class="front__pines"><strong>'+ state.data.data.counts.followers +'</strong> Seguidores</p>');s
     
-    //front.append(left);
     front.append(titleboard);
     front.append(pines);
     front.append(follow);
     front.append(imguserprinc);
-    front.append(prueba);
     
     return front;
 }
